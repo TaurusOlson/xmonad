306f1de542376ff2e9f8762045ede993ac41dc49
Taurus Olson <taurusolson@gmail.com>
2012-05-01 18:32:34 +0200

    initial config

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-xmonad
-======
+# DESCRIPTION
 
-My xmonad configuration
+This is my xmonad configuration based on [Vic Fryzel's xmonad-config](http://github.com/vicfryzel/xmonad-config).
+
+
+# INSTALLATION
+
+* Clone the repository 
+
+    git clone http://github.com/TaurusOlson/xmonad.git ~/.xmonad
+
+
+* Set the xinitrc file and copy the startup file for xmonad
+
+    cd ~/.xmonad
+    make 
+
+
+# REQUIREMENTS
+
+* xmobar
+* dmenu
+* Pragmata (font)
+
---
+++ xmobar.hs
@@ -0,0 +1,22 @@
+-- xmobar config used by Vic Fryzel
+-- Author: Vic Fryzel
+-- http://github.com/vicfryzel/xmonad-config
+
+-- This is setup for dual 1920x1080 monitors, with the right monitor as primary
+Config {
+    font = "xft:Pragmata-10",
+    bgColor = "#151515",
+    fgColor = "#c3c2c5",
+    -- position = Static { xpos = 1920, ypos = 0, width = 1800, height = 16 },
+    lowerOnStart = True,
+    commands = [
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %H:%M" "date" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{  %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>"
+}
