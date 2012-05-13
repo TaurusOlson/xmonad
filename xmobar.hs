b2f8eb93ae54e9f7ead5740841c130dd4b4f50c5
Taurus Olson <taurusolson@gmail.com>
2012-05-13 16:27:13 +0200

    xmobar: Mem Disk Date

--- xmobar.hs
+++ xmobar.hs
@@ -11,12 +11,15 @@ Config {
     lowerOnStart = True,
     commands = [
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        -- Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        -- Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Date "%a %b %_d %H:%M" "date" 10,
+        Run DiskU [("/", "Disk: <used>/<size>")]
+                  ["-L", "20", "-H", "100", "-m", "1", "-p", "3", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{  %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>"
+    -- template = "%StdinReader% }{  %memory% %disku%  %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>"
+    template = "%StdinReader% }{  %memory%    %disku%    <fc=#FFFFCC>%date%</fc>"
 }
